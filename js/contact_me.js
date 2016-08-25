8c00fb11e0b946763ebe976a84c046378dfc79a5
nenick <kuchler.nico@gmail.com>
2016-08-25 09:02:16 +0200

    try freespree.io to send contact email

--- js/contact_me.js
+++ js/contact_me.js
@@ -20,7 +20,7 @@ $(function() {
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
-            $.ajax({
+            /*$.ajax({
                 url: "././mail/contact_me.php",
                 type: "POST",
                 data: {
@@ -54,7 +54,7 @@ $(function() {
                     //clear all fields
                     $('#contactForm').trigger("reset");
                 },
-            });
+            });*/
         },
         filter: function() {
             return $(this).is(":visible");
